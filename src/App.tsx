bcffe0ee2d83e6c617b89781beb5dff6dd1d8171
anjalipatelfablead <anjalipatel.fablead@gmail.com>
2026-02-16 13:55:35 +0530

    added 404 not foud page for wrong routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './components/user/login';
 import Register from './components/user/register';
 import TaskList from './components/task/tasklist';
 import AddTask from './components/task/addtask';
+import NotFound from './components/pages/notFound';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -17,6 +16,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/task" element={<TaskList />} />
           <Route path="/task/add" element={<AddTask />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/pages/notFound.tsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import notfoundbg from "../../assets/404page.jpg"
+
+export default function NotFound() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="min-h-screen flex items-center justify-center ">
+
+            <div className="flex flex-col items-center text-center space-y-6">
+
+                <img
+                    src={notfoundbg}
+                    alt="404 Not Found"
+                    className="w-3xl md:w-3xl "
+                />
+
+                <button
+                    onClick={() => navigate("/login")}
+                    className="mt-4 bg-pink-500 hover:bg-pink-600 text-white px-8 py-3 rounded-xl font-semibold text-lg shadow-lg hover:scale-105 transition-transform duration-300 cursor-pointer"
+                >
+                    Go Back Home
+                </button>
+            </div>
+        </div>
+    );
+
+}
